e30c7d00584772485f64587a151f76a5c4b58ad3
Mugilan <mugilancodes@gmail.com>
2020-05-18 22:06:49 +0530

    Check Again

--- exercises/01-js-drum-kit/js/script.js
+++ exercises/01-js-drum-kit/js/script.js
@@ -1,12 +1,24 @@
-window.addEventListener('keydown', (e) => {
+// ! keypress switches on playing class permanently
+
+const playSound = (e) => {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
   const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
 
   if (!audio) return;
 
-  audio.currentTime = 0;
-
-  audio.play();
-
   key.classList.add('playing');
-});
+
+  audio.currentTime = 0;
+  audio.play();
+};
+
+const removeTransition = (e) => {
+  if (e.propertyName !== 'transform') return;
+
+  e.target.classList.remove('playing');
+};
+
+const keys = document.querySelectorAll('.key');
+keys.forEach((key) => key.addEventListener('transitionend', removeTransition));
+
+window.addEventListener('keydown', playSound);
