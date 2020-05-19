0c4f92749a8ca75084bca7b28bf0ef5f773bea3d
Mugilan <mugilancodes@gmail.com>
2020-05-19 12:27:31 +0530

    Fix needed

--- exercises/02 -js-css-clock/js/script.js
+++ exercises/02 -js-css-clock/js/script.js
@@ -22,11 +22,17 @@ const setDate = () => {
   const minutes = now.getMinutes();
   const minutesDeg = (calcDegree(minutes, 60) + (secondsDeg - 90)) / 60;
   minuteHand.style.transform = `rotate(${minutesDeg}deg)`;
+  // if (minutesDeg === 9.9) {
+  //   minuteHand.classList.add('no-transition');
+  // } else if (minutesDeg === 4.1) {
+  //   minuteHand.classList.remove('no-transition');
+  // }
 
   // todo Hour Hand Glitch
   const hours = now.getHours();
   const hoursDeg = (calcDegree(hours, 12) + (minutesDeg - 90)) / 12;
   hourHand.style.transform = `rotate(${hoursDeg}deg)`;
+  // console.log(hoursDeg);
 };
 
 setInterval(setDate, 1000);
