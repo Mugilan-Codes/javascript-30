0b0f450284c859c7277f522f5da7afd28440d047
Mugilan <mugilancodes@gmail.com>
2020-05-21 18:09:27 +0530

    Type Ahead Fetch Endpoint

--- exercises/06-type-ahead/js/script.js
+++ exercises/06-type-ahead/js/script.js
@@ -1,3 +1,7 @@
+//todo: Change to One of the Two API's
+//* 1. https://rapidapi.com/natkapral/api/countries-cities
+//* 2. https://rapidapi.com/wirefreethought/api/geodb-cities
+
 const endpoint =
   'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
 
@@ -9,7 +13,29 @@ fetch(endpoint)
 
 const findMatches = (wordToMatch, cities) => {
   return cities.filter((place) => {
-    const regex = new RegExp(wordToMatch, 'gi');
+    const regex = new RegExp(wordToMatch, 'gi'); // g (global), i (case insenstive)
     return place.city.match(regex) || place.state.match(regex);
   });
 };
+
+function displayMatches() {
+  const matchArray = findMatches(this.value, cities);
+  const html = matchArray
+    .map(({ city, state, population }) => {
+      return `
+        <li>
+          <span class="name">${city}, ${state}</span>
+          <span className="population">${population}</span>
+        </li>
+      `;
+    })
+    .join('');
+
+  suggestions.innerHTML = html;
+}
+
+const searchInput = document.querySelector('.search');
+const suggestions = document.querySelector('.suggestions');
+
+searchInput.addEventListener('change', displayMatches);
+searchInput.addEventListener('keyup', displayMatches);
