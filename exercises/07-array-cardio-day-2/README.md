df699c696f6478fd5450bf880d26cf0a2e26fa5b
Mugilan <mugilancodes@gmail.com>
2020-05-21 20:52:53 +0530

    Basic Script

---
+++ exercises/07-array-cardio-day-2/README.md
@@ -0,0 +1,3 @@
+# 07 - Array Cardio Day 2
+
+## <a href='https://github.com/Mugilan-Codes/javascript-30'>Main Page</a>
--- exercises/07-array-cardio-day-2/js/script.js
+++ exercises/07-array-cardio-day-2/js/script.js
@@ -1,12 +1,25 @@
-// ## Array Cardio Day 2
+const lineBreak = '==============================================';
 
+console.log(lineBreak);
+
+console.info('Array of Objects of People');
 const people = [
   { name: 'Wes', year: 1988 },
   { name: 'Kait', year: 1986 },
   { name: 'Irv', year: 1970 },
   { name: 'Lux', year: 2015 },
 ];
+console.log(people);
 
+// Some and Every Checks
+// Array.prototype.some()
+console.info('is at least one person 19 or older? (some())');
+// Array.prototype.every()
+console.info('is everyone 19 or older? (every())');
+
+console.log(lineBreak);
+
+console.info('Array of Objects of Comments');
 const comments = [
   { text: 'Love this!', id: 523423 },
   { text: 'Super good', id: 823423 },
@@ -14,15 +27,15 @@ const comments = [
   { text: 'Ramen is my fav food ever', id: 123523 },
   { text: 'Nice Nice Nice!', id: 542328 },
 ];
-
-// Some and Every Checks
-// Array.prototype.some() // is at least one person 19 or older?
-// Array.prototype.every() // is everyone 19 or older?
+console.log(comments);
 
 // Array.prototype.find()
 // Find is like filter, but instead returns just the one you are looking for
-// find the comment with the ID of 823423
+console.info('find the comment with the ID of 823423 (find())');
 
 // Array.prototype.findIndex()
 // Find the comment with this ID
 // delete the comment with the ID of 823423
+console.info('delete the comment with the ID of 823423 (findIndex())');
+
+console.log(lineBreak);
