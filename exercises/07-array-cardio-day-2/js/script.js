afebcf32a4371fdb76d6e97350546e27c8d25edc
Mugilan <mugilancodes@gmail.com>
2020-05-21 21:15:45 +0530

    Array Cardio Day 2

--- exercises/07-array-cardio-day-2/js/script.js
+++ exercises/07-array-cardio-day-2/js/script.js
@@ -9,13 +9,23 @@ const people = [
   { name: 'Irv', year: 1970 },
   { name: 'Lux', year: 2015 },
 ];
-console.log(people);
+console.table(people);
 
 // Some and Every Checks
 // Array.prototype.some()
 console.info('is at least one person 19 or older? (some())');
+const oneIsAdult = people.some(({ year }) => {
+  const currentYear = new Date().getFullYear();
+  return currentYear - year >= 19;
+});
+console.log({ oneIsAdult });
 // Array.prototype.every()
 console.info('is everyone 19 or older? (every())');
+const allAreAdult = people.every(({ year }) => {
+  const currentYear = new Date().getFullYear();
+  return currentYear - year >= 19;
+});
+console.log({ allAreAdult });
 
 console.log(lineBreak);
 
@@ -27,15 +37,26 @@ const comments = [
   { text: 'Ramen is my fav food ever', id: 123523 },
   { text: 'Nice Nice Nice!', id: 542328 },
 ];
-console.log(comments);
+console.table(comments);
 
 // Array.prototype.find()
 // Find is like filter, but instead returns just the one you are looking for
 console.info('find the comment with the ID of 823423 (find())');
+const comment = comments.find(({ id }) => id === 823423);
+console.log(comment);
 
 // Array.prototype.findIndex()
 // Find the comment with this ID
 // delete the comment with the ID of 823423
 console.info('delete the comment with the ID of 823423 (findIndex())');
+const index = comments.findIndex(({ id }) => id === 823423);
+console.log({ index });
+
+// ! This deletes the comment and modifies the original array
+// comments.splice(index, 1);
+
+// * This method creates a new array of comments
+const newComments = [...comments.slice(0, index), ...comments.slice(index + 1)];
+console.table(newComments);
 
 console.log(lineBreak);
