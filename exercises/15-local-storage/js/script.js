0626614d33cf6d12fedebdb014a20d896394ad2b
Mugilan <mugilancodes@gmail.com>
2020-05-25 12:04:33 +0530

    Local Storage Refactor

--- exercises/15-local-storage/js/script.js
+++ exercises/15-local-storage/js/script.js
@@ -1,6 +1,9 @@
 const addItems = document.querySelector('.add-items');
 const itemsList = document.querySelector('.plates');
 const items = JSON.parse(localStorage.getItem('items')) || [];
+const clearItems__button = document.querySelector('.clear-items');
+const clearSelection__button = document.querySelector('.clear-selection');
+const selectAll__button = document.querySelector('.select-all');
 
 const populateList = function (plates = [], platesList) {
   platesList.innerHTML = plates
@@ -49,3 +52,22 @@ addItems.addEventListener('submit', addItem);
 itemsList.addEventListener('click', toggleDone);
 
 populateList(items, itemsList);
+
+clearItems__button.addEventListener('click', () => {
+  // items.splice(0, items.length);
+  items.length = 0;
+  populateList(items, itemsList);
+  localStorage.setItem('items', JSON.stringify(items));
+});
+
+clearSelection__button.addEventListener('click', () => {
+  items.forEach((item) => (item.done = false));
+  populateList(items, itemsList);
+  localStorage.setItem('items', JSON.stringify(items));
+});
+
+selectAll__button.addEventListener('click', () => {
+  items.forEach((item) => (item.done = true));
+  populateList(items, itemsList);
+  localStorage.setItem('items', JSON.stringify(items));
+});
