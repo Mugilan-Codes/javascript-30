c6e5a8f49f603a1f322dd8e8ab5765408e608f77
Mugilan <mugilancodes@gmail.com>
2020-05-26 17:59:48 +0530

    Add Red Effect

--- exercises/19-webcam-fun/js/script.js
+++ exercises/19-webcam-fun/js/script.js
@@ -24,6 +24,12 @@ function paintToCanvas() {
 
   return setInterval(() => {
     ctx.drawImage(video, 0, 0, width, height);
+
+    let pixels = ctx.getImageData(0, 0, width, height);
+
+    pixels = redEffect(pixels);
+
+    ctx.putImageData(pixels, 0, 0);
   }, 16);
 }
 
@@ -37,6 +43,15 @@ function takePhoto() {
   link.setAttribute('download', 'beautiful');
   link.innerHTML = `<img src="${data}" alt="Beautiful Person" />`;
   strip.insertBefore(link, strip.firstChild);
+}
+
+function redEffect(px) {
+  for (let i = 0; i < px.data.length; i += 4) {
+    px.data[i + 0] = px.data[i + 0] + 100; // r
+    px.data[i + 1] = px.data[i + 1] - 50; // g
+    px.data[i + 2] = px.data[i + 2] * 0.5; // b
+  }
+  return px;
 }
 
 getVideo();
