87d74992d701f43aeefcb3a1ab90d2652a54dea1
Mugilan <mugilancodes@gmail.com>
2020-05-26 17:10:16 +0530

    Take Photo

--- exercises/19-webcam-fun/js/script.js
+++ exercises/19-webcam-fun/js/script.js
@@ -30,6 +30,15 @@ function paintToCanvas() {
 function takePhoto() {
   snap.currenTime = 0;
   snap.play();
+
+  const data = canvas.toDataURL('image/jpeg');
+  const link = document.createElement('a');
+  link.href = data;
+  link.setAttribute('download', 'beautiful');
+  link.innerHTML = `<img src="${data}" alt="Beautiful Person" />`;
+  strip.insertBefore(link, strip.firstChild);
 }
 
 getVideo();
+
+video.addEventListener('canplay', paintToCanvas);
