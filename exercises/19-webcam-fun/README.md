42489ac1e0ed30bcfcac0c942eac20ce79a6e274
Mugilan <mugilancodes@gmail.com>
2020-05-26 16:40:58 +0530

    Separated Day 19 from Global

--- exercises/19-webcam-fun/README.md
+++ exercises/19-webcam-fun/README.md
@@ -1,3 +1,9 @@
 # 19 - Unreal Webcam Fun
 
 ## <a href='https://github.com/Mugilan-Codes/javascript-30'>Main Page</a>
+
+#### "start": "browser-sync start --server --files \"_.css, _.html, \*.js\""
+
+#### "browser-sync": "^2.12.5 <2.23.2",
+
+#### video.src = window.URL.createObjectURL(localMediaStream);
---
+++ exercises/19-webcam-fun/js/script.js
@@ -0,0 +1,17 @@
+const video = document.querySelector('.player');
+const canvas = document.querySelector('.photo');
+const ctx = canvas.getContext('2d');
+const strip = document.querySelector('.strip');
+const snap = document.querySelector('.snap');
+
+function getVideo() {
+  navigator.mediaDevices
+    .getUserMedia({ video: true, audio: false })
+    .then((localMediaStream) => {
+      console.log(localMediaStream);
+      video.srcObject = localMediaStream;
+      video.play();
+    });
+}
+
+getVideo();
