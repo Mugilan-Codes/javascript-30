77d552f169eb665f308a7aecd01927dc541d39f8
Mugilan <mugilancodes@gmail.com>
2020-05-26 21:56:22 +0530

    Refactor Code

--- exercises/18-add-with-reduce/js/script.js
+++ exercises/18-add-with-reduce/js/script.js
@@ -1,18 +1,37 @@
-const timeNodes = Array.from(document.querySelectorAll('[data-time]'));
+const reduced__span = document.querySelector('.reduced');
 
-const totalSeconds = timeNodes
-  .map((node) => node.dataset.time)
-  .map((timeCode) => {
-    const [mins, secs] = timeCode.split(':').map(parseFloat);
-    return mins * 60 + secs;
-  })
-  .reduce((total, seconds) => total + seconds);
+const displayTimes = () => {
+  const timeData__li = Array.from(document.querySelectorAll('.videos li'));
 
-let secondsLeft = totalSeconds;
-const hours = Math.floor(secondsLeft / 3600);
-secondsLeft %= 3600;
+  timeData__li.map((li) => {
+    const display__span = li.querySelector('.display');
+    display__span.innerHTML = li.dataset.time;
+  });
 
-const minutes = Math.floor(secondsLeft / 60);
-secondsLeft %= 60;
+  calculateTotalTime();
+};
 
-console.log(hours, minutes, secondsLeft);
+const calculateTotalTime = () => {
+  const timeNodes = Array.from(document.querySelectorAll('[data-time]'));
+
+  const totalSeconds = timeNodes
+    .map((node) => node.dataset.time)
+    .map((timeCode) => {
+      const [mins, secs] = timeCode.split(':').map(parseFloat);
+      return mins * 60 + secs;
+    })
+    .reduce((total, seconds) => total + seconds);
+
+  let secondsLeft = totalSeconds;
+  const hours = Math.floor(secondsLeft / 3600);
+  secondsLeft %= 3600;
+
+  const minutes = Math.floor(secondsLeft / 60);
+  secondsLeft %= 60;
+
+  console.log(hours, minutes, secondsLeft);
+
+  reduced__span.innerHTML = `${hours}:${minutes}:${secondsLeft}`;
+};
+
+displayTimes();
