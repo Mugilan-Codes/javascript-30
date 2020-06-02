38d5e2a255e6c08d52bde6b3dea9ec5039272a43
Mugilan <mugilancodes@gmail.com>
2020-06-02 19:09:08 +0530

    Game Timer

--- exercises/30-whack-a-mole/js/script.js
+++ exercises/30-whack-a-mole/js/script.js
@@ -1,3 +1,42 @@
 const holes = document.querySelectorAll('.hole');
 const scoreBoard = document.querySelector('.score');
 const moles = document.querySelectorAll('.mole');
+
+let lastHole;
+let timeUp = false;
+
+const randomTime = (min, max) => {
+  return Math.round(Math.random() * (max - min) + min);
+};
+
+const randomHole = (holes) => {
+  const idx = Math.floor(Math.random() * holes.length);
+  const hole = holes[idx];
+
+  if (hole === lastHole) {
+    console.log("That's the same one");
+    return randomHole(holes);
+  }
+
+  lastHole = hole;
+  return hole;
+};
+
+const peep = () => {
+  const time = randomTime(200, 1000);
+  const hole = randomHole(holes);
+
+  hole.classList.add('up');
+  setTimeout(() => {
+    hole.classList.remove('up');
+    if (!timeUp) peep();
+  }, time);
+};
+
+const startGame = (gameTime = 10) => {
+  gameTime *= 1000;
+  scoreBoard.textContent = 0;
+  timeUp = false;
+  peep();
+  setTimeout(() => (timeUp = true), gameTime);
+};
